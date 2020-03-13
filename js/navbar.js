5fb7e5a4b470cd8d46eaf3427644237435a13c81
Konrad Czechowski <k.czechowski@interia.eu>
2020-03-13 21:51:40 +0100

    js navbar added, fix in css

---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+let nav = '<nav></nav><ul class="nav justify-content-center"><li class="nav-item"><a href="index.html">Home</a></li>' +
+    '<li class="nav-item"><a href="quiz.html">Quiz</a></li>' +
+    '<li class="nav-item"><a href="contact.html">Kontakt</a></li></ul></nav>';
+
+let element = document.getElementsByClassName("navbar")[0];
+element.innerHTML = nav;
